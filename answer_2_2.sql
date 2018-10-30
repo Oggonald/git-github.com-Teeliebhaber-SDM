348a1bc1898d462e0a545b9fb48da5ba9ec49fa4
Oggonald <berg.egeun@hotmail.de>
2018-10-30 16:42:37 +0100

    Aufgabe 2.1 und 2.2
    gg

---
+++ answer_2_2.sql
@@ -0,0 +1,71 @@
+---------------------
+--   Problem 1 :   --
+---------------------
+.shell echo "Problem 1:"
+-- write sql below --
+
+
+---------------------
+--   Problem 2 :   --
+---------------------
+.shell echo "\nProblem 2:"
+-- write sql below --
+
+
+---------------------
+--   Problem 3 :   --
+---------------------
+.shell echo "\nProblem 3:"
+-- write sql below --
+
+
+---------------------
+--   Problem 4 :   --
+---------------------
+.shell echo "\nProblem 4:"
+-- write sql below --
+
+
+---------------------
+--   Problem 5 :   --
+---------------------
+.shell echo "\nProblem 5:"
+-- write sql below --
+
+
+---------------------
+--   Problem 6 :   --
+---------------------
+.shell echo "\nProblem 6:"
+-- write sql below --
+
+
+---------------------
+--   Problem 7 :   --
+---------------------
+.shell echo "\nProblem 7:"
+-- write sql below --
+
+
+---------------------
+--   Problem 8 :   --
+---------------------
+.shell echo "\nProblem 8:"
+-- write sql below --
+
+
+---------------------
+--   Problem 9 :   --
+---------------------
+.shell echo "\nProblem 9:"
+-- write sql below --
+
+
+---------------------
+--  Extra Credit : --
+---------------------
+.shell echo "\nProblem 10:"
+-- write sql below --
+
+
+---------------------
