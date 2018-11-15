8d0c7042105f80def751113078768b7442e79109
Arsal Ali <arsal.ali@sap.com>
2018-11-15 20:01:11 +0100

    Reformat Code

--- src/test/java/de/tuda/dmdb/access/TestHeapTable.java
+++ src/test/java/de/tuda/dmdb/access/TestHeapTable.java
@@ -9,46 +9,49 @@ import de.tuda.dmdb.storage.types.exercise.SQLInteger;
 import de.tuda.dmdb.storage.types.exercise.SQLVarchar;
 import de.tuda.dmdb.TestCase;
 
-public class TestHeapTable extends TestCase{
-	/**
-	 * Insert 2 records into a heap table and read them by their RID (RowIdentifier)
-	 */
-	public void testTable1(){
-		AbstractRecord record1 = new Record(2);
-		record1.setValue(0, new SQLInteger(1));
-		record1.setValue(1, new SQLVarchar("Hello111", 10));
-		
-		AbstractRecord record2 = new Record(2);
-		record2.setValue(0, new SQLInteger(2));
-		record2.setValue(1, new SQLVarchar("Hello112", 10));
-		
-		AbstractTable table = new HeapTable(record1.clone());
-		RecordIdentifier rid1 = table.insert(record1);
-		RecordIdentifier rid2 = table.insert(record2);
-		
-		AbstractRecord record1Cmp = table.lookup(rid1.getPageNumber(), rid1.getSlotNumber());
-		AbstractRecord record2Cmp = table.lookup(rid2.getPageNumber(), rid2.getSlotNumber());
-		
-		Assert.assertEquals(record1, record1Cmp);
-		Assert.assertEquals(record2, record2Cmp);
-	}
+public class TestHeapTable extends TestCase {
+    public static final int TIMES_TO_INSERT = 3000;
+
+    /**
+     * Insert 2 records into a heap table and read them by their RID (RowIdentifier)
+     */
+    public void testTable1() {
+        AbstractRecord record1 = new Record(2);
+        record1.setValue(0, new SQLInteger(1));
+        record1.setValue(1, new SQLVarchar("Hello111", 10));
+
+        AbstractRecord record2 = new Record(2);
+        record2.setValue(0, new SQLInteger(2));
+        record2.setValue(1, new SQLVarchar("Hello112", 10));
+
+        AbstractTable table = new HeapTable(record1.clone());
+        RecordIdentifier rid1 = table.insert(record1);
+        RecordIdentifier rid2 = table.insert(record2);
+
+        AbstractRecord record1Cmp = table.lookup(rid1.getPageNumber(), rid1.getSlotNumber());
+        AbstractRecord record2Cmp = table.lookup(rid2.getPageNumber(), rid2.getSlotNumber());
+
+        Assert.assertEquals(record1, record1Cmp);
+        Assert.assertEquals(record2, record2Cmp);
+    }
+
     /**
      * Test if a new page is created after filling the first, should happen after 2973 entries
      * have been already entered. Entry with one int and one varchar of 10 bytes
      */
-    public void testNewPageIsCreatedWhenFull() {
-        AbstractRecord record1 = new Record(2);
-        record1.setValue(0, new SQLInteger(1337));
-        record1.setValue(1, new SQLVarchar("1234567890", 10));
+    public void testHeapTablePenentration() {
+        AbstractRecord samplePage = new Record(2);
+        samplePage.setValue(0, new SQLInteger(1188));
+        samplePage.setValue(1, new SQLVarchar("MotorolaG4", 10));
 
-        AbstractTable table = new HeapTable(record1.clone());
-        for(int i = 0; i < 2974; i++) {
-            table.insert(record1.clone());
+        AbstractTable testTable = new HeapTable(samplePage.clone());
+        for (int i = 0; i < TIMES_TO_INSERT; i++) {
+            testTable.insert(samplePage.clone());
         }
-        int pageSize = ((HeapTable) table).pages.size();
-        int numRecordsOnLastPage = ((HeapTable) table).lastPage.getNumRecords();
+        int sizeOfPage = ((HeapTable) testTable).pages.size();
+        int recordsOnLastPage = ((HeapTable) testTable).lastPage.getNumRecords();
 
-        assertEquals(2, pageSize);
-        assertEquals(1, numRecordsOnLastPage);
+        assertEquals(2, sizeOfPage);
+        assertEquals(27, recordsOnLastPage);
     }
 }
